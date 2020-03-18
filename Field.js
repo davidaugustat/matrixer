4f3acdc1e458f177027911984fd994538cf1d03a
David Augustat <code@davidaugustat.com>
2020-03-18 03:49:55 +0100

    Added templates for field lookup tables

--- Field.js
+++ Field.js
@@ -12,6 +12,30 @@ class Field{
   static F19 = 19;
   static R = 100; // real numbers
 
+  static F4Zero = 0;
+  static F4One = 1;
+  static F4Alpha = 2;
+  static F4AlphaPlusOne = 3;
+
+  static F8Zero = 0;
+  static F8One = 1;
+  static F8Beta = 2;
+  static F8OnePlusBeta = 3;
+  static F8BetaSquare = 4;
+  static F8OnePlusBetaSquare = 5;
+  static F8BetaPlusBetaSquare = 6;
+  static F8OnePlusBetaPlusBetaSquare = 7;
+
+  static F9Zero = 0;
+  static F9One = 1;
+  static F9MinusOne = 2;
+  static F9Iota = 3;
+  static F9IotaPlusOne = 4;
+  static F9IotaMinusOne = 5;
+  static F9MinusIota = 6;
+  static F9MinusIotaPlusOne = 7;
+  static F9MinusIotaMinusOne = 8;
+
   field;
   constructor(field){
     this.field = field;
